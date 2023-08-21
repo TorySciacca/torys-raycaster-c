c99b5337d3ca15f5d5e6de2ec8804466d5c065e0
TorySciacca <57762089+TorySciacca@users.noreply.github.com>
2023-08-21 16:28:09 +1000

    testing final ray cast

--- code.c
+++ code.c
@@ -4,6 +4,10 @@
 #include <math.h>
 #define PI 3.14159265359
 
+//------------------------PLAYER------------------------------------------------
+float degToRad(int a) { return a*M_PI/180.0;}
+int FixAng(int a){ if(a>359){ a-=360;} if(a<0){ a+=360;} return a;}
+
 float px,py,pdx,pdy,pa; //player position
 
 void drawPlayer()
@@ -54,35 +58,82 @@ void drawMap2D()
  }
 }
 
+//---------------------------Draw Rays and Walls--------------------------------
+float distance(ax,ay,bx,by,ang){ return cos(degToRad(ang))*(bx-ax)-sin(degToRad(ang))*(by-ay);}
+
+
 void drawRays3D()
 {
- int r,mx,my,mp,dof; float rx,ry,ra,xo,yo;
- ra=pa;
- for(r=0;r<1;r++)
+ glColor3f(0,1,1); glBegin(GL_QUADS); glVertex2i(526,  0); glVertex2i(1006,  0); glVertex2i(1006,160); glVertex2i(526,160); glEnd();	
+ glColor3f(0,0,1); glBegin(GL_QUADS); glVertex2i(526,160); glVertex2i(1006,160); glVertex2i(1006,320); glVertex2i(526,320); glEnd();	 	
+	
+ int r,mx,my,mp,dof,side; float vx,vy,rx,ry,ra,xo,yo,disV,disH; 
+ 
+ ra=FixAng(pa+30);                                                              //ray set back 30 degrees
+ 
+ for(r=0;r<60;r++)
  {
- 	//----Check Horizontal Lines---
- 	dof=0;
- 	float aTan=-1/tan(ra);
- 	if(ra>PI){ ry((int)py>>6)>>6)-0.0001; rx=(py-ry)*aTan+px; yo=-64; xo=-yo*aTan;} //looking up (dividing by 64 via bit shifting)
- 	if(ra>PI){ ry((int)py>>6)>>6)+64    ; rx=(py-ry)*aTan+px; yo=-64; xo=-yo*aTan;} 
+  //---Vertical--- 
+  dof=0; side=0; disV=100000;
+  float Tan=tan(degToRad(ra));
+       if(cos(degToRad(ra))> 0.001){ rx=(((int)px>>6)<<6)+64;      ry=(px-rx)*Tan+py; xo= 64; yo=-xo*Tan;}//looking left
+  else if(cos(degToRad(ra))<-0.001){ rx=(((int)px>>6)<<6) -0.0001; ry=(px-rx)*Tan+py; xo=-64; yo=-xo*Tan;}//looking right
+  else { rx=px; ry=py; dof=8;}                                                  //looking up or down. no hit  
+
+  while(dof<8) 
+  { 
+   mx=(int)(rx)>>6; my=(int)(ry)>>6; mp=my*mapX+mx;                     
+   if(mp>0 && mp<mapX*mapY && map[mp]==1){ dof=8; disV=cos(degToRad(ra))*(rx-px)-sin(degToRad(ra))*(ry-py);}//hit         
+   else{ rx+=xo; ry+=yo; dof+=1;}                                               //check next horizontal
+  } 
+  vx=rx; vy=ry;
+
+  //---Horizontal---
+  dof=0; disH=100000;
+  Tan=1.0/Tan; 
+       if(sin(degToRad(ra))> 0.001){ ry=(((int)py>>6)<<6) -0.0001; rx=(py-ry)*Tan+px; yo=-64; xo=-yo*Tan;}//looking up 
+  else if(sin(degToRad(ra))<-0.001){ ry=(((int)py>>6)<<6)+64;      rx=(py-ry)*Tan+px; yo= 64; xo=-yo*Tan;}//looking down
+  else{ rx=px; ry=py; dof=8;}                                                   //looking straight left or right
+ 
+  while(dof<8) 
+  { 
+   mx=(int)(rx)>>6; my=(int)(ry)>>6; mp=my*mapX+mx;                          
+   if(mp>0 && mp<mapX*mapY && map[mp]==1){ dof=8; disH=cos(degToRad(ra))*(rx-px)-sin(degToRad(ra))*(ry-py);}//hit         
+   else{ rx+=xo; ry+=yo; dof+=1;}                                               //check next horizontal
+  } 
+  
+  glColor3f(0,0.8,0);
+  if(disV<disH){ rx=vx; ry=vy; disH=disV; glColor3f(0,0.6,0);}                  //horizontal hit first
+  glLineWidth(2); glBegin(GL_LINES); glVertex2i(px,py); glVertex2i(rx,ry); glEnd();//draw 2D ray
+    
+  int ca=FixAng(pa-ra); disH=disH*cos(degToRad(ca));                            //fix fisheye 
+  int lineH = (mapS*320)/(disH); if(lineH>320){ lineH=320;}                     //line height and limit
+  int lineOff = 160 - (lineH>>1);                                               //line offset
+  
+  glLineWidth(8);glBegin(GL_LINES);glVertex2i(r*8+530,lineOff);glVertex2i(r*8+530,lineOff+lineH);glEnd();//draw vertical wall  
+
+  ra=FixAng(ra-1);                                                              //go to next ray
  }
-}
+}//-----------------------------------------------------------------------------
+
 
 void display()
 {
  glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
  drawMap2D();
+ drawRays3D();
  drawPlayer();
  glutSwapBuffers();
 }
-
-void buttons(unsigned char key, int x, int y)
+ 
+ 
+void buttons(unsigned char key,int x,int y)
 {
-	if(key=='a') { pa-=0.1; if(pa<   0){ pa+=2*PI;} pdx=cos(pa)*5; pdy=sin(pa)*5;}
-	if(key=='d') { pa+=0.1; if(pa<2*PI){ pa-=2*PI;} pdx=cos(pa)*5; pdy=sin(pa)*5;} 
-	if(key=='w') { px+=pdx; py+=pdy;}
-	if(key=='s') { px-=pdx; py-=pdy;}
-	glutPostRedisplay();				
+ if(key=='a'){ pa+=5; pa=FixAng(pa); pdx=cos(degToRad(pa)); pdy=-sin(degToRad(pa));} 	
+ if(key=='d'){ pa-=5; pa=FixAng(pa); pdx=cos(degToRad(pa)); pdy=-sin(degToRad(pa));} 
+ if(key=='w'){ px+=pdx*5; py+=pdy*5;}
+ if(key=='s'){ px-=pdx*5; py-=pdy*5;}
+ glutPostRedisplay();				
 }
 
 void init ()
